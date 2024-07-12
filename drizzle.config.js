f81c548094c4171c32c5594f74cc3b0affff65b3
Neeljain <nj6097114@gmail.com>
2024-07-13 00:23:20 +0530

    form

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,6 @@ export default {
     schema: "./utils/schema.js",
     dialect: 'postgresql',
     dbCredentials: {
-      url: "postgresql://aidb_owner:qU5VIvTdDe0k@ep-lucky-haze-a5854zsg.us-east-2.aws.neon.tech/ai-mock?sslmode=require",
+      url:DRIZZLE_DATABASE_URL,
     }
   };
