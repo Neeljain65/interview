970bf22748b29645c4928f1fd3e0419caeb70b8f
Neel jain <139386481+Neeljain65@users.noreply.github.com>
2025-07-05 18:12:32 +0530

    Update page.jsx

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function page() {
   return (
     <div>
-        <h1 className='text-5xl font-bold text-center'>Nigga You gotta focus on Your interviews not on us</h1>
+        <h1 className='text-3xl font-bold text-center'>At Mocky-Interview, we aim to revolutionize interview preparation by offering personalized, AI-powered mock interviews. Simply enter your job role and description, and our smart system generates tailored questions, evaluates your answers, and provides instant feedback. Whether you're a fresher or a professional, Mocky-Interview helps you build confidence and improve with every attempt. </h1>
     </div>
   )
 }
